890fda1eefe65ea46ec0a9f96142b8afaf958258
shiho-k <shii0327@yahoo.co.jp>
2024-07-07 13:38:11 +0900

    Lesson 6Chapter 10 ここまでで作成したコードのコミット

---
+++ java.lang/src/javaapis/math/MathClassSample.java
@@ -0,0 +1,10 @@
+package javaapis.math;
+
+public class MathClassSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
---
+++ javaapis.datetime/src/javaapis/datetime/DateSample02.java
@@ -0,0 +1,19 @@
+package javaapis.datetime;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateSample02 {
+
+    public static void main(String[] args) {
+        LocalDateTime dateTime=LocalDateTime.now();
+
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyy/MM/dd");
+        String str=dateTime.format(formatter);
+        System.out.println(str);
+
+
+
+    }
+
+}
